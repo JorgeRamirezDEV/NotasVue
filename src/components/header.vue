a6ac4599523166c7892e22dc42b5fc93940b7d8a
Jorge Ramírez <72385150+JorgeRamirezDEV@users.noreply.github.com>
2021-02-08 22:14:07 +0100

    .

---
+++ src/components/header.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+
+  <section class="cabecera">
+    <h1>Recordatorio Notas</h1>
+  </section>
+
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'header',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped>
+  .header {
+    text-align: center;
+  }
+</style>
